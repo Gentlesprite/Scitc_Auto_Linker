42ff2ff20c13e2a1bb7afb0103dd144517943f6c
LZY/我不是盘神 <2286502367@qq.com>
2023-09-25 23:18:47 +0800

    获取管理员权限,目的是为了有足够在开机目录创建自动化脚本

--- admin.py
+++ admin.py
@@ -10,7 +10,7 @@ def is_admin():  # 判断当前是否为管理员
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
-        # ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, __file__, None, 1)  # 自动重启获得管理员
+        # ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, __file__, None, 1) # 自动重启获得管理员
         return False
 
 # print(bool(is_admin()))
